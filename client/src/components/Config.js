f4131094fd3b15b62fa3dae6f06c08e2de263a91
_zz_hyun <jhhhj1002@hanmail.net>
2020-10-07 00:35:42 +0900

    update

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -7,8 +7,8 @@ const Auctions = require('../contracts/Auctions')
 const RealEstate = require('../contracts/RealEstate.json')
 
 export default {
-	MYNFT_CA: '0x49356ddE00771Db532b80e6014bd9FDFBf2B7Ae8',//migrate할 때 주소 (사람마다 다시 할 때마다 다름)
-	AUCTIONS_CA: '0x24269a37bfb237089f8484bb565F864f0003f93e',
+	MYNFT_CA: '0x1E281b852cFCf64A529d335D5b917542eAB2FA5F',//migrate할 때 주소 (사람마다 다시 할 때마다 다름)
+	AUCTIONS_CA: '0x3C264eb3187519d93e898907E0aa3D9520d01E2b',
 	REALSTATE_CA: '',
 	
 	MYNFT_ABI: MyNFT.abi,
